8798b847f0ca36d8fc2d262f3535a72fb7792497
bee <rkddbtjd123@gmail.com>
2022-06-16 14:19:50 +0900

    oop project init

---
+++ oop/src/main/kotlin/me/spring/oop/OopApplication.kt
@@ -0,0 +1,11 @@
+package me.spring.oop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OopApplication
+
+fun main(args: Array<String>) {
+    runApplication<OopApplication>(*args)
+}
