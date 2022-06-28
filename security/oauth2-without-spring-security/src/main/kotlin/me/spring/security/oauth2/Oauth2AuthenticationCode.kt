13d332a7cf79742ac3c455f72b330df524fcc7e6
bee <rkddbtjd123@gmail.com>
2022-06-28 14:23:31 +0900

    feat: change Oauth2AuthenticationCode resourceServer property access modifier

--- security/oauth2-without-spring-security/src/main/kotlin/me/spring/security/oauth2/Oauth2AuthenticationCode.kt
+++ security/oauth2-without-spring-security/src/main/kotlin/me/spring/security/oauth2/Oauth2AuthenticationCode.kt
@@ -1,6 +1,6 @@
 package me.spring.security.oauth2
 
 class Oauth2AuthenticationCode(
-    private val resourceServer: ResourceServer,
-    val authenticationCode: String
+    val resourceServer: ResourceServer,
+    val code: String
 )
