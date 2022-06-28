a073603d4896bb5898491005547b76c3c36d45e9
bee <rkddbtjd123@gmail.com>
2022-06-28 14:30:59 +0900

    test: add test that default Oauth2Authentication CodeFactory::getOauth2AuthenticationCodeBy returns oauth2 auth code via Google auth code redirect

--- security/oauth2-without-spring-security/src/test/kotlin/me/spring/security/oauth2/DefaultOauth2AuthenticationCodeFactoryTest.kt
+++ security/oauth2-without-spring-security/src/test/kotlin/me/spring/security/oauth2/DefaultOauth2AuthenticationCodeFactoryTest.kt
@@ -1,44 +1,59 @@
 package me.spring.security.oauth2
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.mock.web.MockHttpServletRequest
+
 internal class DefaultOauth2AuthenticationCodeFactoryTest {
 
-    private val defaultOauth2AuthenticationCodeFactory = DefaultOauth2AuthenticationCodeFactory(
-        hashMapOf(
-            "google" to ResourceServer(
-                clientName = "google",
-                clientId = "googleClientId",
-                clientSecret = "googleClientSecret",
-                userInfoUrl = "googleUserInfoUrl",
-                resourceServerId = "googleResourceServerId",
-                userInfoAttributes = ResourceServer.UserInfoAttributes(
-                    profile = "googleProfile",
-                    email = "googleEmail"
-                )
-            ),
-            "naver" to ResourceServer(
-                clientName = "naver",
-                clientId = "naverClientId",
-                clientSecret = "naverClientSecret",
-                userInfoUrl = "naverUserInfoUrl",
-                resourceServerId = "naverResourceServerId",
-                userInfoAttributes = ResourceServer.UserInfoAttributes(
-                    profile = "naverProfile",
-                    email = "naverEmail"
-                )
-            ),
-            "kakao" to ResourceServer(
-                clientName = "kakao",
-                clientId = "kakaoClientId",
-                clientSecret = "kakaoClientSecret",
-                userInfoUrl = "kakaoUserInfoUrl",
-                resourceServerId = "kakaoResourceServerId",
-                userInfoAttributes = ResourceServer.UserInfoAttributes(
-                    profile = "kakaoProfile",
-                    email = "kakaoEmail"
-                )
+    private val clients = hashMapOf(
+        "google" to ResourceServer(
+            clientName = "google",
+            clientId = "googleClientId",
+            clientSecret = "googleClientSecret",
+            userInfoUrl = "googleUserInfoUrl",
+            resourceServerId = "googleResourceServerId",
+            userInfoAttributes = ResourceServer.UserInfoAttributes(
+                profile = "googleProfile",
+                email = "googleEmail"
+            )
+        ),
+        "naver" to ResourceServer(
+            clientName = "naver",
+            clientId = "naverClientId",
+            clientSecret = "naverClientSecret",
+            userInfoUrl = "naverUserInfoUrl",
+            resourceServerId = "naverResourceServerId",
+            userInfoAttributes = ResourceServer.UserInfoAttributes(
+                profile = "naverProfile",
+                email = "naverEmail"
+            )
+        ),
+        "kakao" to ResourceServer(
+            clientName = "kakao",
+            clientId = "kakaoClientId",
+            clientSecret = "kakaoClientSecret",
+            userInfoUrl = "kakaoUserInfoUrl",
+            resourceServerId = "kakaoResourceServerId",
+            userInfoAttributes = ResourceServer.UserInfoAttributes(
+                profile = "kakaoProfile",
+                email = "kakaoEmail"
             )
         )
     )
 
+    private val defaultOauth2AuthenticationCodeFactory = DefaultOauth2AuthenticationCodeFactory(
+        clients
+    )
+
+    @Test
+    fun getOauth2AuthenticationCodeBy_return_oauth2AuthenticationCode_through_googleAuthenticationCodeRedirect() {
+        val servletRequest = MockHttpServletRequest("GET", "/auth/google")
+        val googleAuthenticationCode = "googleAuthenticationCode"
+        servletRequest.addParameter("code", googleAuthenticationCode)
+        val authenticationCode = defaultOauth2AuthenticationCodeFactory.getOauth2AuthenticationCodeBy(servletRequest)
+        assertThat(authenticationCode.resourceServer).isEqualTo(clients["google"])
+        assertThat(authenticationCode.code).isEqualTo(googleAuthenticationCode)
+    }
 
 }
