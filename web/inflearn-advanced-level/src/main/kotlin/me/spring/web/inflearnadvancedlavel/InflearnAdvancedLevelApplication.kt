da077cdc9021908f4c00d1ed16cb508120db5eff
bee <rkddbtjd123@gmail.com>
2022-07-04 17:35:15 +0900

    feat: 인프런 스프링 핵심원리 고급편 프로젝트 추가

---
+++ web/inflearn-advanced-level/src/main/kotlin/me/spring/web/inflearnadvancedlavel/InflearnAdvancedLevelApplication.kt
@@ -0,0 +1,11 @@
+package me.spring.web.inflearnadvancedlavel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InflearnAdvancedLevelApplication
+
+fun main(args: Array<String>) {
+    runApplication<InflearnAdvancedLevelApplication>(*args)
+}
