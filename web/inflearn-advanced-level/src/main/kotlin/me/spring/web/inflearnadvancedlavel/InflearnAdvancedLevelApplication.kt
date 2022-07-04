8aededd9b267ba97fddd9ebf7da1ba49f4edf1cc
bee <rkddbtjd123@gmail.com>
2022-07-05 00:39:26 +0900

    feat: DynamicProxyConfig 추가

--- web/inflearn-advanced-level/src/main/kotlin/me/spring/web/inflearnadvancedlavel/InflearnAdvancedLevelApplication.kt
+++ web/inflearn-advanced-level/src/main/kotlin/me/spring/web/inflearnadvancedlavel/InflearnAdvancedLevelApplication.kt
@@ -2,15 +2,14 @@ package me.spring.web.inflearnadvancedlavel
 
 import me.spring.web.inflearnadvancedlavel.proxy.app.trace.logtrace.LogTrace
 import me.spring.web.inflearnadvancedlavel.proxy.app.trace.logtrace.ThreadLocalLogTrace
-import me.spring.web.inflearnadvancedlavel.proxy.config.AppV1Config
 import me.spring.web.inflearnadvancedlavel.proxy.config.AppV2Config
-import me.spring.web.inflearnadvancedlavel.proxy.config.InterfaceProxyConfig
+import me.spring.web.inflearnadvancedlavel.proxy.config.DynamicProxyConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 
-@Import(InterfaceProxyConfig::class, AppV2Config::class)
+@Import(DynamicProxyConfig::class, AppV2Config::class)
 @SpringBootApplication(scanBasePackages = ["me.spring.web.inflearnadvancedlavel.proxy.app"])
 class InflearnAdvancedLevelApplication {
 
