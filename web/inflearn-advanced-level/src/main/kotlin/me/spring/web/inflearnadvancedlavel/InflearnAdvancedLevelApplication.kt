c5539f8f8adb0274f563e0fb4e7226ee5fae67c7
bee <rkddbtjd123@gmail.com>
2022-07-04 17:59:42 +0900

    feat: app으로 이동

--- web/inflearn-advanced-level/src/main/kotlin/me/spring/web/inflearnadvancedlavel/InflearnAdvancedLevelApplication.kt
+++ web/inflearn-advanced-level/src/main/kotlin/me/spring/web/inflearnadvancedlavel/InflearnAdvancedLevelApplication.kt
@@ -7,7 +7,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
 @Import(AppV1Config::class, AppV2Config::class)
-@SpringBootApplication(scanBasePackages = ["me.spring.web.inflearnadvancedlavel.proxy.v1"])
+@SpringBootApplication(scanBasePackages = ["me.spring.web.inflearnadvancedlavel.proxy.app"])
 class InflearnAdvancedLevelApplication
 
 fun main(args: Array<String>) {
