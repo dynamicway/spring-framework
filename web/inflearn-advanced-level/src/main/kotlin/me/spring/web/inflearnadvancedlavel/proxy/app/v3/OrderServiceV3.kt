bc5662ef92b3802a2b27d5c13ace1c082c31b9fe
bee <rkddbtjd123@gmail.com>
2022-07-04 18:01:27 +0900

    feat: v3 추가

---
+++ web/inflearn-advanced-level/src/main/kotlin/me/spring/web/inflearnadvancedlavel/proxy/app/v3/OrderServiceV3.kt
@@ -0,0 +1,13 @@
+package me.spring.web.inflearnadvancedlavel.proxy.app.v3
+
+import org.springframework.stereotype.Service
+
+@Service
+class OrderServiceV3(
+    private val orderRepository: OrderRepositoryV3
+) {
+
+    fun orderItem(itemId: String) {
+        orderRepository.save(itemId)
+    }
+}
