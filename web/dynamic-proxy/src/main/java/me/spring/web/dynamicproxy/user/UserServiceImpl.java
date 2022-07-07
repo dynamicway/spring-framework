cd23c7d2afa90cc17a684022edc7f58422347f2f
bee <rkddbtjd123@gmail.com>
2022-07-07 17:50:59 +0900

    refactor: UserServiceImpl의 생성자를 lombok으로 구현하도록 수정

--- web/dynamic-proxy/src/main/java/me/spring/web/dynamicproxy/user/UserServiceImpl.java
+++ web/dynamic-proxy/src/main/java/me/spring/web/dynamicproxy/user/UserServiceImpl.java
@@ -1,17 +1,14 @@
 package me.spring.web.dynamicproxy.user;
 
-
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public String getUserName(long userId) {
