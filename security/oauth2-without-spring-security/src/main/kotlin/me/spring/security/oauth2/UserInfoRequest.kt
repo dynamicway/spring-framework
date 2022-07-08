9eaa432b4a75b555a4ba712961a71baa797e9c94
bee <rkddbtjd123@gmail.com>
2022-07-08 16:14:14 +0900

    feat: UserInfoRequest 추가

---
+++ security/oauth2-without-spring-security/src/main/kotlin/me/spring/security/oauth2/UserInfoRequest.kt
@@ -0,0 +1,16 @@
+package me.spring.security.oauth2
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.RequestEntity
+
+class UserInfoRequest(
+    private val httpMethod: HttpMethod,
+    private val uri: String
+) {
+    fun getAccessTokenRequestEntity(accessToken: String): RequestEntity<Void> {
+        return RequestEntity.method(httpMethod, uri)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
+                .build()
+    }
+}
