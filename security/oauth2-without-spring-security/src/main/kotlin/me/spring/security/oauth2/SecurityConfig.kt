d073c9804c25a32f5e7cf3bacb2f8c20ee129d79
bee <rkddbtjd123@gmail.com>
2022-07-09 20:12:30 +0900

    feat: SecurityConfig 추가 - AuthenticationFilter 등록

--- security/oauth2-without-spring-security/src/main/kotlin/me/spring/security/oauth2/SecurityConfig.kt
+++ security/oauth2-without-spring-security/src/main/kotlin/me/spring/security/oauth2/SecurityConfig.kt
@@ -1,9 +1,25 @@
 package me.spring.security.oauth2
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationPropertiesScan(basePackageClasses = [Oauth2AuthenticationCodeFactory::class])
+@ConfigurationPropertiesScan(basePackageClasses = [ResourceServerRequestFactory::class])
 class SecurityConfig {
+
+    @Bean
+    fun oauth2AuthenticationCodeFilter(
+        resourceServerRequestFactory: ResourceServerRequestFactory,
+        oauth2UserProvider: Oauth2UserProvider
+    ) = FilterRegistrationBean<Oauth2AuthenticationCodeFilter>()
+            .apply {
+                filter = Oauth2AuthenticationCodeFilter(
+                    resourceServerRequestFactory,
+                    oauth2UserProvider
+                )
+                urlPatterns = listOf("/auth/*")
+            }
+
 }
