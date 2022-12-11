d3f8974ce3ef50885542ee2fc93de13eff026e14
bee <rkddbtjd123@gmail.com>
2022-12-11 22:19:45 +0900

    feat: Implementing UserApi::ageMustNotBeNull to pass UserApiTest::validate_query_parameters test

--- web/validation/src/main/java/me/spring/web/validation/user/UserApi.java
+++ web/validation/src/main/java/me/spring/web/validation/user/UserApi.java
@@ -1,17 +1,27 @@
 package me.spring.web.validation.user;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController
+@Validated
 public class UserApi {
 
     @PostMapping("/by-Valid-annotation-on-controller-method")
     public void byValidAnnotationOnControllerMethod(@Valid @RequestBody User user) {
 
     }
+
+    @GetMapping("/age-must-not-be-null")
+    public void ageMustNotBeNull(@NotNull Integer age) {
+
+    }
+
 
 }
